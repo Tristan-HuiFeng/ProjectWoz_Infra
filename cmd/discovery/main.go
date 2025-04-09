adda04daad8608bd8aae7109db62b63654b57513
Chew Hui Feng <tristan.chf@gmail.com>
2025-04-09 18:29:16 +0800

    draft

--- cmd/discovery/main.go
+++ cmd/discovery/main.go
@@ -167,13 +167,13 @@ func handler(ctx context.Context) error {
 
 	log.Info().Msg("running interval discovery")
 
-	awsClientID := "1"
-	awsAccountID := "050752608470"
+	// awsClientID := "1"
+	// awsAccountID := "050752608470"
 	clientEmail := "user.ad.proj@gmail.com"
 	clientGCPProjectID := "the-other-450607-a4"
 	// clientGCPProjectID := "cs464-454011"
 
-	awsHandler(awsClientID, awsAccountID, clientEmail)
+	// awsHandler(awsClientID, awsAccountID, clientEmail)
 
 	gcpHandler(clientGCPProjectID, clientEmail)
 
